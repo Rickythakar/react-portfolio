fcd69c1996079bac87e85d64485a3a68a3126005
Ricky Thakar <rickythakar@gmail.com>
2022-06-04 12:30:25 -0400

    hooked up nav page to app.js and refactored code

---
+++ react-portfolio/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+      </p>
+    </div>
+  );
+}
