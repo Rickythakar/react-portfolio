e6ca28b870a877047dcb574d35280d4e2f310854
Ricky Thakar <rickythakar@gmail.com>
2022-06-07 22:41:32 -0400

    updated conact me form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
-      <p>
-        You can reach me at the following links
-        {/* <br>
-          <a href='https://www.linkedin.com/in/ricky-thakar-4a82231a2/'>LinkedIn</a>
-        </br> */}
-      </p>
+      <h2></h2>
+      <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      Please enter your name and email 
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="name" placeholder="Name" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
     </div>
   );
 }
